55e967d6dcfe65076871741b31298a5bbb02de63
b3n3dict <benedict.xavier.e@gmail.com>
2020-04-23 11:50:00 +0530

    updated

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-// var database = [
-// 	{
-// 		username: "andrei",
-// 		password: "supersecret"
-// 	},{
-// 		username: "ben",
-// 		password: "admin"
-// 	},{
-// 		username: "bony",
-// 		password: "bony123"
-// 	}
-// ];
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	},{
+		username: "ben",
+		password: "admin"
+	},{
+		username: "bony",
+		password: "bony123"
+	}
+];
 
-// var newsfeed = [
-// 	{
-// 		username: "Bobby",
-// 		timeline: "So tired from all that learning!"
-// 	},
-// 	{
-// 		username: "Sally",
-// 		timeline: "Javascript is sooooo cool!"
-// 	},
-// 	{
-// 		username: "Mitch",
-// 		timeline: "Javascript is preeetyy cool!"
-// 	}
-// ];
-// function isUserValid(username,password){
-// 	for(var i=0;i<database.length;i++){
-// 		if(database[i].username===username &&
-// 			database[i].password===password){
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+function isUserValid(username,password){
+	for(var i=0;i<database.length;i++){
+		if(database[i].username===username &&
+			database[i].password===password){
 			
-// 			return true;
-// 		}
-// 		}
-// 		return false;
-// 	}
-// function login(username,password){
-// 	if(isUserValid(username,password)){
-// 		console.log(newsfeed);
-// 	}else
-// 	{
-// 			alert("entered username and password is wrong!");
-// }
-// }
+			return true;
+		}
+		}
+		return false;
+	}
+function login(username,password){
+	if(isUserValid(username,password)){
+		console.log(newsfeed);
+	}else
+	{
+			alert("entered username and password is wrong!");
+}
+}
 
-// var userNamePropmt = prompt("enter your user name:");
-// var userPassPrompt = prompt("enter your password:");
+var userNamePropmt = prompt("enter your user name:");
+var userPassPrompt = prompt("enter your password:");
 
-// login(userNamePropmt,userPassPrompt);
+login(userNamePropmt,userPassPrompt);
 
